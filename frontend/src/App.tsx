e78ef77c32c56eb877a8bb8adf4b9fef679e6266
regan nguyen <regankelowna@gmail.com>
2026-09-06 17:53:30 -0700

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+//main screen
+
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>SourceLearn</h1>
+      <p>asking question about document</p>
+    </div>
+   
+  )
+}
+
+export default App
